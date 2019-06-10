e0b491d8f0287cdd656a48b4be2d61e3b1144762
pradipto.roy <pradipto.roy@INKOL-2-431A.concentrix.com>
2019-06-10 19:22:19 +0530

    update

---
+++ angular-springboot-mysql/src/main/java/com/roytechworld/angularspringbootmysql/AngularSpringbootMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.roytechworld.angularspringbootmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootMysqlApplication.class, args);
+	}
+
+}
---
+++ angular-springboot-mysql/src/main/java/com/roytechworld/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.roytechworld.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
+
+@Controller
+@RequestMapping("user")
+public class HomeController {
+	
+/*	
+@PostMapping("home")
+public ResponseBody void testController()
+{
+
+	
+}
+*/
+}
